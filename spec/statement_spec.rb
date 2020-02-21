1a6f0e6c00d29a27bbb75bc36bc70d393f9caf36
yuji91 <yuji91.1227@gmail.com>
2020-02-21 13:55:28 +0900

    Add spec for statement method

---
+++ spec/statement_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe  do
+  require './movie.rb'
+  require './rental.rb'
+  require './customer.rb'
+
+  it 'is correct to output result from statement method' do
+    movie = Movie.new("movie_01", Movie::REGULAR)
+    rental = Rental.new(movie, 0)
+    customer = Customer.new("customer_01")
+
+    customer.add_rental(rental)
+
+    result = customer.statement
+    expected_result = "Rental Record for customer_01 \n\tmovie_01\t2\nAmount owed is 2\nYou earned 1 frequent renter points"
+    expect(result).to match expected_result
+  end
+end
