05e7b7b5e8957b49d4048d721c526cadbe7e6689
yuji91 <yuji91.1227@gmail.com>
2020-02-29 16:53:51 +0900

    Self Encapsulate Field

--- movie.rb
+++ movie.rb
@@ -7,8 +7,12 @@ class Movie
   attr_reader :title
   attr_accessor :price_code
 
-  def initialize(title, price_code)
-    @title, @price_code = title, price_code
+  def price_code=(value)
+    @price_code = value
+  end
+
+  def initialize(title, the_price_code)
+    @title, self.price_code = title, the_price_code
   end
 
   def charge(days_rented)
