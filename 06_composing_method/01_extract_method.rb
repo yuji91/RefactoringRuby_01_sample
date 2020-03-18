dd4bdc4b2504a8c3cffa2111662853493d263f8a
yuji91 <yuji91.1227@gmail.com>
2020-03-18 15:41:37 +0900

    01_メソッドの抽出

--- 06_composing_method/01_extract_method.rb
+++ 06_composing_method/01_extract_method.rb
@@ -1,7 +1,12 @@
 # 6.1 メソッドの抽出
 # 1つにまとめられるコードの断片がある
+# -> コードの断片をメソッドにして、その目的を説明する名前を付ける。
 def print_owing(amount)
   print_banner
+  print_details(amount)
+end
+
+def print_details(amount)
   puts "name: #{@name}"
   puts "amount #{amount}"
 end
