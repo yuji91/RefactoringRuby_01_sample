1b3cfbf5233933ede21dfb9587ed4d3d0d75d9c4
yuji91 <yuji91.1227@gmail.com>
2020-03-21 12:29:48 +0900

    03: 一時変数のインライン化

--- 06_composing_method/03_inline_temp.rb
+++ 06_composing_method/03_inline_temp.rb
@@ -1,4 +1,4 @@
 # 6.3 一時変数のインライン化
 # 単純な式で1度だけ代入されている一時変数があり、その一時変数が他のリファクタリングの邪魔になっている。
-base_price = an_order.base_price
-return (base_price > 1000)
+# -> その一時変数に対するすべての参照を取り除き、式にする。
+return (an_order.base_price > 1000)
