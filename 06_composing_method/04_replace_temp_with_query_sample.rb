fd775009496fb46ef50a48b161bff753528f4f2e
yuji91 <yuji91.1227@gmail.com>
2020-03-21 13:06:57 +0900

    04: sample 最初の一時変数参照をメソッド呼び出しに書き換える

--- 06_composing_method/04_replace_temp_with_query_sample.rb
+++ 06_composing_method/04_replace_temp_with_query_sample.rb
@@ -1,8 +1,10 @@
 # 6.4 一時変数から問い合わせメソッドへ : サンプル
 # 1度に1つずつ変数を置き換えていく。まず、代入の右辺を抜き出してメソッドにまとめる。
+# ここでテストをしてから、「一時変数のインライン化」を始める。
+# まず、最初の一時変数参照をメソッド呼び出しに書き換える。
 def price
   a_base_price = base_price
-  if a_base_price > 1000
+  if base_price > 1000
     discount_factor = 0.95
   else
     discount_factor = 0.98
