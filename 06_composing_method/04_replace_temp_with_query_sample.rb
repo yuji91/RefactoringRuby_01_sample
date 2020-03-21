5c2174632b56e846a5f9f7bead91804c2bb18b75
yuji91 <yuji91.1227@gmail.com>
2020-03-21 13:09:32 +0900

    04: sample 次を置換して、一時変数への代入を削除

--- 06_composing_method/04_replace_temp_with_query_sample.rb
+++ 06_composing_method/04_replace_temp_with_query_sample.rb
@@ -2,14 +2,15 @@
 # 1度に1つずつ変数を置き換えていく。まず、代入の右辺を抜き出してメソッドにまとめる。
 # ここでテストをしてから、「一時変数のインライン化」を始める。
 # まず、最初の一時変数参照をメソッド呼び出しに書き換える。
+# テストしてから、次を置換する(呼び出し元が一列に並んで踊るような感じである)。
+# これが最後なので、一時変数への代入も削除する。
 def price
-  a_base_price = base_price
   if base_price > 1000
     discount_factor = 0.95
   else
     discount_factor = 0.98
   end
-  a_base_price * discount_factor
+  base_price * discount_factor
 end
 
 def base_price
