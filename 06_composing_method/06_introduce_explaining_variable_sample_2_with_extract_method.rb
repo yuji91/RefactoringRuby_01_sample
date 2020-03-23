fad0d8aafb62eb1a1445ed2e4a2655a7a8f6fc5b
yuji91 <yuji91.1227@gmail.com>
2020-03-23 23:03:16 +0900

    06: sample_2 送料割引の計算部分もメソッドに置き換える

--- 06_composing_method/06_introduce_explaining_variable_sample_2_with_extract_method.rb
+++ 06_composing_method/06_introduce_explaining_variable_sample_2_with_extract_method.rb
@@ -2,13 +2,18 @@
 # このサンプルの場合、通常の私なら説明用変数を導入しただけでは終わりにはしない。
 # メソッドの抽出で同じことをするだろう。また同じコードからスタートしよう。
 # しかし、今度は基本価格計算をメソッドにする。
+# 同じことを1度に1つずつ行う。
 def price
   # 価格は、基本価格 - 送料割引 + 配送料
   return base_price -
-      [0, @quantity - 500].max * @item_price * 0.05 +
+      quantity_discount +
       [base_price * 0.1, 100.0].min
 end
 
 def base_price
   @quantity * @item_price
+end
+
+def quantity_discount
+  [0, @quantity - 500].max * @item_price * 0.05
 end
