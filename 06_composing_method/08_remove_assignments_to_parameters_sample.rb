3ebf557205417aa7c0eb28fcc2372e49379e872e
yuji91 <yuji91.1227@gmail.com>
2020-03-23 23:35:13 +0900

    08: sample 引数を一時変数に置き換える

--- 06_composing_method/08_remove_assignments_to_parameters_sample.rb
+++ 06_composing_method/08_remove_assignments_to_parameters_sample.rb
@@ -1,7 +1,9 @@
 # 6.8 引数への代入の除去 : サンプル
+# 引数を一時変数に置き換えると、次のようになる。
 def discount(input_val, quantity, year_to_date)
-  input_val -= 2 if input_val > 50
-  input_val -= 1 if quantity > 100
-  input_val -= 4 if year_to_date > 10000
-  input_val
+  result = input_val
+  result -= 2 if input_val > 50
+  result -= 1 if quantity > 100
+  result -= 4 if year_to_date > 10000
+  result
 end
