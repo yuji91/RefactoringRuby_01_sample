fb85d1f57599025403a94f10f8869b45bc9cd84d
yuji91 <yuji91.1227@gmail.com>
2020-03-23 23:20:12 +0900

    07: sample 次の代入までの一時変数の名前を変更する

--- 06_composing_method/07_split_temporart_variable_sample.rb
+++ 06_composing_method/07_split_temporart_variable_sample.rb
@@ -4,12 +4,12 @@
 # しばらくすると、第2の力を受けてハギスに加速が付く。
 # 運動の一般法則を使えば、ハギスの移動距離は次のように計算できる。
 def distance_traveled(time)
-  acc = @primary_force / @mass
+  primary_acc = @primary_force / @mass
   primary_time = [time, @delay].min
-  result = 0.5 * acc * primary_time * primary_time
+  result = 0.5 * primary_acc * primary_time * primary_time
   secondary_time = time - @delay
   if (secondary_time > 0)
-    primary_vel = acc * @delay
+    primary_vel = primary_acc * @delay
     acc = (@primary_force + @secondary_force) / @mass
     result += primary_vel * secondary_time + 5 * acc * secondary_time * secondary_time
   end
@@ -18,3 +18,7 @@ end
 # acc変数は2つの仕事を持っている。
 # 最初の力を受けた結果得た最初の加速度を保存することと、2つの力を受けた後の加速度を保存することである。
 # そこで、この2つを分割する。
+# まず、一時変数の名前を変更する。
+# 次に、その位置から次の代入までの範囲に現れる一時変数の名前を同じように変える。
+# 新しい名前として、一時変数の第1の用途だけを表すものを選んでいる。
+# テストは成功するはずである。
