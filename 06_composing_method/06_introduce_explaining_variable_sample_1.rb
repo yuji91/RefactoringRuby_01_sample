f93da092d1274d8476e9abbf3a331b5727707d91
yuji91 <yuji91.1227@gmail.com>
2020-03-23 14:57:57 +0900

    06: sample_1 数量割引の計算部分も一時変数に置き換える

--- 06_composing_method/06_introduce_explaining_variable_sample_1.rb
+++ 06_composing_method/06_introduce_explaining_variable_sample_1.rb
@@ -4,10 +4,12 @@
 # まず、基本価格(base_price)は、単価(@item_price)の数量(@quantity)倍である。
 # 計算のその部分を一時変数に置き換える。
 # 単価の数量倍は後でも使われるので、そこでも一時変数を使う。
+# 次に数量割引(quantity_discount)を一時変数にする。
 def price
   # 価格は、基本価格 - 送料割引 + 配送料
   base_price = @quantity * @item_price
+  quantity_discount = [0, @quantity - 500].max * @item_price * 0.05
   return base_price -
-      [0, @quantity - 500].max * @item_price * 0.05 +
+      quantity_discount +
       [base_price * 0.1, 100.0].min
 end
