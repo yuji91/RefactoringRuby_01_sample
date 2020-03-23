7ed84ee79b9a8960e2a643969e2e48b6bc851f9e
yuji91 <yuji91.1227@gmail.com>
2020-03-23 14:42:03 +0900

    06: 説明用変数の導入

--- 06_composing_method/06_introduce_explaining_variable.rb
+++ 06_composing_method/06_introduce_explaining_variable.rb
@@ -1,9 +1,9 @@
 # 6.6 説明用変数の導入
 # 複雑な式がある。
-if (platform.upcase.index("MAC") &&
-    browser.upcase.index("IE") &&
-    initialized? &&
-    resize > 0
-)
+# -> 処理の目的を説明するような名前を持つ一時変数に式、または式の一部の結果を代入する。
+is_mac_os = platform.upcase.index("MAC")
+is_ie_browser = browser.upcase.index("IE")
+was_resized = resize > 0
+if (is_mac_os && is_ie_browser && initialized? && was_resized)
   # 何かをする
 end
