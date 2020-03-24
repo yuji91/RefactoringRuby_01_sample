d7ac978c1bb4b6c39c03c98e0005ce542e20f9ad
yuji91 <yuji91.1227@gmail.com>
2020-03-24 11:01:10 +0900

    08: sample オブジェクト参照に代入しても値渡しされているため、代入の効果がスコープ外では反映されない例を追加

--- 06_composing_method/08_remove_assignments_to_parameters_sample.rb
+++ 06_composing_method/08_remove_assignments_to_parameters_sample.rb
@@ -20,3 +20,47 @@ puts "x after triple #{x}"
 # 次のように出力する。
 # arg in triple: 15
 # x after triple 5
+#
+# 混乱が起きるのは、オブジェクトの状態を変えるメソッドを呼び出せるためである。
+class Ledger
+  attr_reader :balance
+
+  def initialize(balance)
+    @balance = balance
+  end
+
+  def add(arg)
+    @balance = arg
+  end
+end
+
+class Product
+  def self.add_price_by_updating(ledger, price)
+    ledger.add(price)
+    puts "ledger in add_price_by_updating: #{ledger.balance}"
+  end
+
+  def self.add_price_by_replacing(ledger, price)
+    ledger = Ledger.new(ledger.balance + price)
+    puts "ledger in add_price_by_replacing: #{ledger.balance}"
+  end
+end
+
+l1 = Ledger.new(0)
+Product.add_price_by_updating(l1, 5)
+puts "l1 after add_price_by_updating: #{l1.balance}"
+
+l2 = Ledger.new(0)
+Product.add_price_by_replacing(l2, 5)
+puts "l2 after add_price_by_replacing: #{l2.balance}"
+
+# このコードは次のように出力する。
+# ledger in add_price_by_updating: 5
+# l1 after add_price_by_updating: 5
+#
+# ledger in add_price_by_replacing: 5
+# l2 after add_price_by_replacing: 0
+#
+# オブジェクト参照は値渡しされる。
+# オブジェクト参照を使ってメソッドを呼び出したり、状態に変更を加えたりすることは可能で、呼び出しスタックの上の方でも変更結果は有効である。
+# しかし、オブジェクト参照に代入をしても、このオブジェクト参照は値渡しされているため、メソッド本体のスコープを離れると、この代入の効果は反映されなくなってしまう。
