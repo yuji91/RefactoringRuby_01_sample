be097b285eacefe4453511db6255461ddbc036b4
yuji91 <yuji91.1227@gmail.com>
2020-03-24 20:38:46 +0900

    09: sample 古いメソッドはメソッドオブジェクトに処理を委ねる

--- 06_composing_method/09_replace_method_with_method_object_sample.rb
+++ 06_composing_method/09_replace_method_with_method_object_sample.rb
@@ -3,14 +3,7 @@
 # このリファクタリングの方法を説明する
 class Account
   def gamma(input_val, quantity, year_to_date)
-    important_value1 = (input_val * quantity) + @account.delta
-    important_value2 = (input_val * year_to_date) + 100
-    if (year_to_date - important_value1) > 100
-      important_value2 -= 20
-    end
-    important_value3 = important_value2 * 7
-    # 等々
-    important_value3 - 2 * important_value1
+    Gamma.new(self, input_val, quantity, year_to_date).compute
   end
 end
 # このメソッドをメソッドオブジェクトに書き換えるために、まず、新しいクラスを宣言する。
@@ -32,4 +25,15 @@ class Gamma
   end
   # これで元のメソッドの受け入れ先ができた。
   # Accountのメンバの使っている部分は、@account インスタンス変数を使うように書き換えなければならない。
+  # そして、古いメソッドは、メソッドオブジェクトに処理を委ねる(委譲する)ように書き換える。
+  def compute
+    @important_value1 = (input_val * quantity) + @account.delta
+    @important_value2 = (input_val * year_to_date) + 100
+    if (year_to_date - important_value1) > 100
+      @important_value2 -= 20
+    end
+    @important_value3 = important_value2 * 7
+    # and so on.
+    @important_value3 - 2 * important_value1
+  end
 end
