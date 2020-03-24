71686f728c612247df6552bd1e92623eb9b34ce2
yuji91 <yuji91.1227@gmail.com>
2020-03-24 20:31:15 +0900

    09: sample コンストラクタを追加

--- 06_composing_method/09_replace_method_with_method_object_sample.rb
+++ 06_composing_method/09_replace_method_with_method_object_sample.rb
@@ -3,7 +3,7 @@
 # このリファクタリングの方法を説明する
 class Account
   def gamma(input_val, quantity, year_to_date)
-    important_value1 = (input_val * quantity) + delta
+    important_value1 = (input_val * quantity) + @account.delta
     important_value2 = (input_val * year_to_date) + 100
     if (year_to_date - important_value1) > 100
       important_value2 -= 20
@@ -23,4 +23,13 @@ class Gamma
               :important_value1,
               :important_value2,
               :important_value3
+  # 次に、コンストラクタを追加する。
+  def initialize(account, input_val_arg, quantity_arg, year_to_date_arg)
+    @account = account
+    @input_val = input_val_arg
+    @quantity = quantity_arg
+    @year_to_date = year_to_date_arg
+  end
+  # これで元のメソッドの受け入れ先ができた。
+  # Accountのメンバの使っている部分は、@account インスタンス変数を使うように書き換えなければならない。
 end
