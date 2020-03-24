26c095d1e89c0051866f4a7c7d99e660fd70bffa
yuji91 <yuji91.1227@gmail.com>
2020-03-24 19:51:33 +0900

    09: メソッドからメソッドオブジェクトへ

--- 06_composing_method/09_replace_method_with_method_object.rb
+++ 06_composing_method/09_replace_method_with_method_object.rb
@@ -1,9 +1,34 @@
 # 6.9 メソッドからメソッドオブジェクトへ
 # 「メソッドの抽出」を適用できないようなローカル変数の使い方をしている長いメソッドがある。
+# -> メソッドを独自のオブジェクトに変え、すべてのローカル変数がそのオブジェクトのインスタンス変数になるようにする。
+#    こうすれば、メソッドを分解して、同じオブジェクトの別のメソッドにすることが可能になる。
 class Order
   def price
-    primary_base_price = 0
-    secondary_base_price = 0
-    tertiary_base_price = 0
+    return PriceCalculator.new(self).compute()
+  end
+end
+
+class Price
+  attr_reader :primary_base_price,
+              :secondary_base_price,
+              :tertiary_base_price
+
+  def initialize(primary_base_price_arg, secondary_base_price_arg, tertiary_base_price_arg)
+    @primary_base_price = primary_base_price_arg
+    @secondary_base_price = secondary_base_price_arg
+    @tertiary_base_price = tertiary_base_price_arg
+  end
+end
+
+class PriceCalculator
+  attr_reader :order
+
+  def initialize(order)
+    @order = order
+  end
+
+  def compute
+    # @orderの内容によって計算時の条件を変える
+    # case @order
   end
 end
